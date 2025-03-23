7fe00757951ffcfc143472392af984d47df0387a
Leonardo Fernandes Mesquita <lfm.fiap@gmail.com>
2025-03-23 13:36:17 -0300

    Add files via upload

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -2,30 +2,46 @@ import React from 'react';
 
 function Produtos() {
   return (
-    <div>
+    <div className="produtos-container">
       <h1>Produtos</h1>
-      <div>
+      <div className="produto-item">
         <h3>Camisa do São Paulo I 25 New Balance Masculina Torcedor</h3>
-        <h4>R$ 332,49</h4>
-        <img src="./src/assets/camisaDoBambi.jpg" alt="Produto 1" style={{ width: '300px', height: '300px' }} />
+        <h4>R$ 332,49</h4>
+        <img
+          src="/assets/camisa-sao-paulo-25-new-balance.jpg"
+          alt="Camisa do São Paulo I 25 New Balance Masculina Torcedor"
+          className="produto-img"
+        />
       </div>
       <hr />
-      <div>
+      <div className="produto-item">
         <h3>Camisa de Treino do São Paulo 25 New Balance</h3>
-        <h4>R$ 208,99</h4>
-        <img src="./src/assets/camisaDoOutroBambi.jpg" alt="Produto 2" style={{ width: '300px', height: '300px' }} />
+        <h4>R$ 208,99</h4>
+        <img
+          src="/assets/camisa-treino-sao-paulo-25-new-balance.jpg"
+          alt="Camisa de Treino do São Paulo 25 New Balance"
+          className="produto-img"
+        />
       </div>
       <hr />
-      <div>
+      <div className="produto-item">
         <h3>Camisa Corinthians II 25 Torcedor Nike Masculina</h3>
-        <h4>R$ 314,99</h4>
-        <img src="./src/assets/CamisaDoMaiorClubeDoMundo.jpg" alt="Produto 3" style={{ width: '300px', height: '300px' }} />
+        <h4>R$ 314,99</h4>
+        <img
+          src="/assets/camisa-corinthians-25-nike.jpg"
+          alt="Camisa Corinthians II 25 Torcedor Nike Masculina"
+          className="produto-img"
+        />
       </div>
       <hr />
-      <div>
+      <div className="produto-item">
         <h3>Camisa Palmeiras Puma I 24/25 Torcedor</h3>
-        <h4>R$ 249,90</h4>
-        <img src="./src/assets/camisaDoSemMundial.jpg" alt="Produto 4" style={{ width: '300px', height: '300px' }} />
+        <h4>R$ 249,90</h4>
+        <img
+          src="/assets/camisa-palmeiras-24-25-puma.jpg"
+          alt="Camisa Palmeiras Puma I 24/25 Torcedor"
+          className="produto-img"
+        />
       </div>
     </div>
   );
